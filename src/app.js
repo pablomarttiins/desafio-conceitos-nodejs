3634a95af48a7083a90319986b6f97db08c8fee7
Pablo Martins Lemos <pablo.martins@hotmail.com.br>
2020-06-18 08:31:16 -0300

    CRUD Projects, Likes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -10,24 +11,78 @@ app.use(cors());
 
 const repositories = [];
 
+function validateIdRepository(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id))
+    return response.status(400).json({error: "Repository ID not found!"});
+
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
   // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+  
+  repositories.push(repository);
+
+  return response.status(200).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
+app.put("/repositories/:id", validateIdRepository, (request, response) => {
   // TODO
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  const currentRepository = repositories[repositoryIndex];
+
+  const updateRepository = {
+    ...currentRepository,
+    url,
+    title,
+    techs
+  }
+  
+  repositories[repositoryIndex] = updateRepository;
+
+  return response.json(updateRepository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
+app.delete("/repositories/:id", validateIdRepository, (request, response) => {
   // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateIdRepository, (request, response) => {
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id == id);
+
+  repository.likes += 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
